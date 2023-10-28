8ede09066039c397833b4fec137f392e0f2bdf6f
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-10-28 12:47:23 -0300

    Adicionando o código para conexão com wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "wifi_esp.h"
+#include "config.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    
+    set_parameters_wifi(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    wifi_loop();
+    delay(1000);
 }
