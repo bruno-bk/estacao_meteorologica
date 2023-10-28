a5dd154e5c6fd0762139e907b8dc1373ba5b14db
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-10-28 17:06:56 -0300

    Realizando a leitura simples do sensor DHT11

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include <Arduino.h>
+#include <DHT.h>
+
 #include "wifi_esp.h"
 #include "mqtt_esp.h"
 #include "config.h"
 
+#define DHTPIN 4
+
+DHT dht(DHTPIN, DHT11);
+
+void read_DHT11() {
+    float h = dht.readHumidity();
+    char humidity[5];
+
+    if (isnan(h)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+    }
+    sprintf(humidity, "%d", (uint32_t)h);
+    Serial.print("humidity: ");
+    Serial.print(humidity);
+    Serial.println("%");
+    send_mqtt_message("station/humidity", humidity);
+}
+
 void setup() {
     Serial.begin(115200);
+
+    dht.begin();
     
     set_parameters_wifi(WIFI_SSID, WIFI_PASSWORD);
     set_parameters_mqtt(MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_PASSWORD);
@@ -13,5 +36,8 @@ void setup() {
 void loop() {
     wifi_loop();
     mqtt_loop();
-    delay(1000);
+
+    read_DHT11();
+
+    delay(5000);
 }
