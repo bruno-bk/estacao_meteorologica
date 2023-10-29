54be23239fbad91351aa0192c103d299efa403f9
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-10-29 10:36:22 -0300

    Realizando a leitura simples do sensor AS5600

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <AS5600.h>
 
 #include "wifi_esp.h"
 #include "mqtt_esp.h"
@@ -8,6 +9,7 @@
 #define DHTPIN 4
 
 DHT dht(DHTPIN, DHT11);
+AS5600 as5600; 
 
 void read_DHT11() {
     float h = dht.readHumidity();
@@ -24,10 +26,24 @@ void read_DHT11() {
     send_mqtt_message("station/humidity", humidity);
 }
 
+void read_as5600() {
+    float a = abs((as5600.rawAngle()*360.0/4095.0));
+    char angle[5];
+
+    sprintf(angle, "%d", (uint32_t)a);
+    Serial.print("Angle: ");
+    Serial.print(angle);
+    Serial.println("°");
+    send_mqtt_message("station/angle", angle);
+}
+
 void setup() {
     Serial.begin(115200);
 
     dht.begin();
+
+    as5600.begin(4);
+    as5600.setDirection(AS5600_CLOCK_WISE);
     
     set_parameters_wifi(WIFI_SSID, WIFI_PASSWORD);
     set_parameters_mqtt(MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_PASSWORD);
@@ -38,6 +54,7 @@ void loop() {
     mqtt_loop();
 
     read_DHT11();
+    read_as5600();
 
     delay(5000);
 }
